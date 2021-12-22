112849224f92111776d16204d798ddffd93ea730
charlesctr <chcoutur@student.42.fr>
2021-12-22 17:43:20 +0100

    Update include with define and prototypes for lib and parsing

---
+++ includes/ft_ssl.h
@@ -0,0 +1,15 @@
+#ifndef FT_SSL_H
+#define FT_SSL_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#define TRUE 1
+#define FALSE 0
+
+
+
+bool parsing(char **argv);
+
+/*          LIBFT       */
+int		ft_strcmp(const char *s1, const char *s2);
+#endif
