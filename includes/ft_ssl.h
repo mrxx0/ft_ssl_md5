ec36bb2cb923e643526a75dbbb255cfd837b0864
charlesctr <chcoutur@student.42.fr>
2022-01-10 14:56:40 +0100

    Update prototype for ft_strlen

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -3,8 +3,8 @@
 
 #include <stdbool.h>
 #include <stdio.h>
-#define TRUE 1
-#define FALSE 0
+#define TRUE 0
+#define FALSE 1
 
 
 
@@ -12,4 +12,5 @@ bool parsing(char **argv);
 
 /*          LIBFT       */
 int		ft_strcmp(const char *s1, const char *s2);
+size_t	ft_strlen(const char *s);
 #endif
