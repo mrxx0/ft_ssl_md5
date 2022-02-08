abbb41b55cd8e80c1954fce7e08f027a3f7343fd
charlesctr <chcoutur@student.42.fr>
2022-02-08 16:14:58 +0100

    Print debug function for ssl structure

--- srcs/main.c
+++ srcs/main.c
@@ -1,11 +1,42 @@
 #include "../includes/ft_ssl.h"
 
+void print_info_ssl(t_ssl *ssl)
+{
+	/*char					*input;
+	char					*output;
+	size_t					input_size;
+    int                     cmd;
+	int						error;
+	int                     flag;*/
+
+	printf("%s\n", "-------------------------------");
+	printf("input = %s\n", ssl->input);
+	printf("output = %s\n", ssl->output);
+	printf("input_size = %zu\n", ssl->input_size);
+	if (ssl->cmd == MD5)
+		printf("cmd = %s\n", "MD5");
+	else if (ssl->cmd == SHA256)
+		printf("cmd = %s\n", "SHA256");
+	else
+		printf("cmd = %d\n", ssl->cmd);
+	printf("error = %d\n", ssl->error);
+	printf("flag = %d\n", ssl->flag);
+
+}
+
 int main(int argc, char **argv)
 {
 	if (argc >= 2)
 	{
-		if (parsing(argv) == FALSE)
+	    t_ssl	*ssl;
+		ssl = init_new_ssl();
+		if (parsing(argv, ssl) == FALSE)
+		{
+			free(ssl);
 			return (FALSE);
+		}
+		print_info_ssl(ssl);
+		free(ssl);
 		return (TRUE);
 	}
 	handle_errors(USAGE, NULL);
