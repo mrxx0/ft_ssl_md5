7714940d442d974b133ae458a547df0a1a76b299
charlesctr <chcoutur@student.42.fr>
2022-02-08 14:30:17 +0100

    Add ft_memalloc in lib

--- srcs/lib.c
+++ srcs/lib.c
@@ -1,5 +1,39 @@
 #include "../includes/ft_ssl.h"
 
+void	*ft_memset(void *b, int c, size_t n)
+{
+	size_t	i;
+	char	*b_cpy;
+
+	b_cpy = (char *)b;
+	i = 0;
+	while (i < n)
+	{
+		b_cpy[i] = (unsigned char)c;
+		i++;
+	}
+	return (b_cpy);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
+
+void	*ft_memalloc(size_t size)
+{
+	char *mem;
+
+	if (!(mem = malloc(sizeof(char) * size)))
+		return (NULL);
+	if (mem)
+	{
+		ft_bzero(mem, size);
+		return (mem);
+	}
+	return (NULL);
+}
+
 int		ft_strcmp(const char *s1, const char *s2)
 {
 	int i;
