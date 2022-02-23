d68b616baa8b27aab47bcb8fb2dc813b10c8a7d6
charlesctr <chcoutur@student.42.fr>
2022-02-23 16:34:22 +0100

    Commnent print ssl

--- srcs/main.c
+++ srcs/main.c
@@ -1,28 +1,28 @@
 #include "../includes/ft_ssl.h"
 
-void print_info_ssl(t_ssl *ssl)
-{
-	/*char					*input;
-	char					*output;
-	size_t					input_size;
-    int                     cmd;
-	int						error;
-	int                     flag;*/
+// void print_info_ssl(t_ssl *ssl)
+// {
+// 	/*char					*input;
+// 	char					*output;
+// 	size_t					input_size;
+//     int                     cmd;
+// 	int						error;
+// 	int                     flag;*/
 
-	printf("%s\n", "-------------------------------");
-	printf("input = %s\n", ssl->input);
-	printf("output = %s\n", ssl->output);
-	printf("input_size = %zu\n", ssl->input_size);
-	if (ssl->cmd == MD5)
-		printf("cmd = %s\n", "MD5");
-	else if (ssl->cmd == SHA256)
-		printf("cmd = %s\n", "SHA256");
-	else
-		printf("cmd = %d\n", ssl->cmd);
-	printf("error = %d\n", ssl->error);
-	printf("flag = %d\n", ssl->flag);
+// 	printf("%s\n", "-------------------------------");
+// 	printf("input = %s\n", ssl->input);
+// 	printf("output = %s\n", ssl->output);
+// 	printf("input_size = %zu\n", ssl->input_size);
+// 	if (ssl->cmd == MD5)
+// 		printf("cmd = %s\n", "MD5");
+// 	else if (ssl->cmd == SHA256)
+// 		printf("cmd = %s\n", "SHA256");
+// 	else
+// 		printf("cmd = %d\n", ssl->cmd);
+// 	printf("offset = %d\n", ssl->offset);
+// 	printf("flag = %d\n", ssl->flag);
 
-}
+// }
 
 int main(int argc, char **argv)
 {
@@ -36,7 +36,8 @@ int main(int argc, char **argv)
 			return (FALSE);
 		}
 		read_input(argv, &argc, ssl);
-		print_info_ssl(ssl);
+		//print_info_ssl(ssl);
+		// delete ssl
 		if (ssl->input)
 			free(ssl->input);
 		free(ssl);
