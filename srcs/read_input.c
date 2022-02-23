dd0340e85ade9799c84c10c0f86604167ca3b983
charlesctr <chcoutur@student.42.fr>
2022-02-23 16:35:04 +0100

    Read file input V1

--- srcs/read_input.c
+++ srcs/read_input.c
@@ -47,25 +47,59 @@ int open_input(t_ssl *ssl, char *input)
     (void)ssl;
 
     if ((fd = open(input, O_RDONLY)) == -1)
-        printf("%s\n", "FAIL OPEN");
+        printf("%s\n", "FAIL OPEN"); //errno
+    // if bad fd, check arg if it's a flag -> error handling
     if (fstat(fd, &buf) == 0 && S_ISDIR(buf.st_mode))
-        printf("%s\n", "FAIL OPEN");
+        printf("%s\n", "FAIL OPEN"); //err isdir
     return (fd);
+}
+
+u_int64_t get_input_size(int fd)
+{
+    struct stat	buf;
+
+	if (fstat(fd, &buf) != 0)
+		return (0);
+	return (buf.st_size / 10);
 }
 
 /*
     Reads input from string/file
 */
 
-void         read_input_file_string(t_ssl *ssl, char *input)
+void         read_input_file(t_ssl *ssl, char *input)
 {
     int fd;
-    // char *buffer;
-    // ssize_t read_return;
-    // u_int64_t buffer_size;
+    char *buffer;
+    ssize_t read_return = 0;
+    u_int64_t buffer_size;
 
     fd = open_input(ssl, input);
-    printf("reading from file string on fd = %d: %s\n", fd, input);
+    buffer_size = get_input_size(fd);
+    if (!(buffer = (char *)ft_memalloc(sizeof(char) * (buffer_size + 1))))
+    {
+        close(fd);
+        handle_errors(MALLOC_FAILED, NULL); // exit
+    }
+    while ((read_return = read(fd, buffer, buffer_size)) > 0)
+    {
+        if (!(ssl->input = strnjoins(
+		ssl->input, buffer, ssl->input_size, read_return)))
+		{
+            printf("%s\n", "FAIL");
+            return;
+        }
+        ssl->input_size += read_return;
+		ft_bzero(buffer, buffer_size);
+    }
+    if (fd > 2)
+        close(fd);
+    if (buffer)
+        free(buffer);
+    // if error malloc empty string
+    // printf("reading from file on fd = %d: %s\n", fd, input);
+    // printf("Size of %s = %lu\n", input, buffer_size);
+    // printf("content = \n%s\n", ssl->input);
     return ;
 }
 
@@ -75,22 +109,23 @@ void         read_input_file_string(t_ssl *ssl, char *input)
 
 bool read_input(char **argv, int *argc, t_ssl *ssl)
 {
-    int i = 2;
+    int i = ssl->offset;
+    printf("offset = %d | argc = %d\n", ssl->offset, *argc);
     // loop to iterate on sting and -s flag
-    while (i <= *argc)
+    if ((ssl->flag & FLAG_P) != 0)
+            read_input_stdin(ssl);
+    else if (ssl->offset == *argc)
+            read_input_stdin(ssl);
+    // Dispatch to hash algorithm
+    // Clear SSL input/input_size//output
+    clear_ssl(ssl);
+    while (i < *argc)
     {
-        if ((ssl->flag & FLAG_P) != 0)
-            read_input_stdin(ssl);
-        else if (*argc == 0)
-            read_input_stdin(ssl);
-        else
-            read_input_file_string(ssl, argv[i]);
-        
+        read_input_file(ssl, argv[i]);
         // Dispatch to hash algorithm
         // Clear SSL input/input_size//output
+        clear_ssl(ssl);
         i++;
     }
-    
-    (void)argv;
     return (TRUE);
 }
