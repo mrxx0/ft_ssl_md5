aceef103fa2e7a9d476cdaebb93d3fa81456cdb7
charlesctr <chcoutur@student.42.fr>
2022-02-23 16:33:52 +0100

    Add clear ssl function

--- srcs/init.c
+++ srcs/init.c
@@ -1,5 +1,17 @@
 #include "../includes/ft_ssl.h"
 
+
+void    clear_ssl(t_ssl *ssl)
+{
+    ssl->input_size = 0;
+    if (ssl->input)
+        free(ssl->input);
+    ssl->input = NULL;
+    if (ssl->output)
+        free(ssl->output);
+    ssl->output = NULL;
+}
+
 /*
     Zeroing ssl structure.
 */
@@ -13,7 +25,7 @@ t_ssl   *init_new_ssl(void)
         handle_errors(MALLOC_FAILED, NULL);
     ssl->input_size = 0;
     ssl->cmd = 0;
-    ssl->error = 0;
+    ssl->offset = 0;
     ssl->flag = 0;
     return (ssl);
 }
