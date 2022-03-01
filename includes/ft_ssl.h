895e11b95bc769c08ff3b3af972facfe443a7f3d
charlesctr <chcoutur@student.42.fr>
2022-03-01 16:08:41 +0100

    Update prototype parsing + add define for invalid file/directory

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -21,6 +21,7 @@
 # define USAGE 3
 # define MALLOC_FAILED 4
 # define REPETED_FLAG 5
+# define INVALID_FILE_DIRECTORY 6
 
 /*      FLAGS CODE     */
 
@@ -46,18 +47,19 @@ typedef struct				s_ssl
     char                    pad[4];
 }							t_ssl;
 
-bool 	parsing(char **argv, int *argc, t_ssl *ssl);
+bool 	parsing(char **argv, t_ssl *ssl);
 void 	handle_errors(int error_id, char *error);
 t_ssl 	*init_new_ssl(void);
 void    clear_ssl(t_ssl *ssl);
 bool 	read_input(char **argv, int *argc, t_ssl *ssl);
 
 /*          LIBFT       */
+
 int		ft_strcmp(const char *s1, const char *s2);
 size_t	ft_strlen(const char *s);
 void	*ft_memalloc(size_t size);
 void	ft_bzero(void *s, size_t n);
 void	ft_strdel(char **as);
 char	*ft_strdup(const char *s1);
-char		*strnjoins(char *s1, char *s2, size_t len1, size_t len2);
+char	*strnjoins(char *s1, char *s2, size_t len1, size_t len2);
 #endif
