6907469b3beffb45b03874a692dce8a49a4e4a13
mrxx0 <chcoutur@student.42.fr>
2022-05-20 17:53:04 +0200

    Remove debug prints

--- srcs/md5.c
+++ srcs/md5.c
@@ -8,12 +8,7 @@ void md5 (char *input, size_t input_size)
 {
     t_md5 *md5;
 
-    (void)input;
-    printf("-> HASING IN MD5 <-\n");
-    printf("-> INIT MD5 <-\n");
     md5 = init_new_md5(input_size);
-    printf("-> STEP MD5 <-\n");
     md5_processing(md5, input);
-    printf("-> CLEAR MD5 <-\n");
     clear_md5(md5);
 }
